ed9c2727fb42976d9998acfcc72bd02b10def6f9
ayaan-waqar <143525054+ayaan-waqar@users.noreply.github.com>
2024-07-09 15:31:54 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace assignment1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sumofnumbers();
+            //evenumbers();
+            //Leapyear();
+            conversion();
+        }
+
+        static void sumofnumbers() 
+        {
+            string sum;
+            Console.WriteLine("enter num1 and num2");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int num3 = num1 + num2; 
+            sum = num3.ToString();
+
+            Console.WriteLine(sum);
+            
+
+
+        }
+
+        static void evenumbers() 
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                    
+            };
+        }
+
+        static void Leapyear() 
+        {
+            Console.WriteLine("enter year");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("it is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("it is not a leap year");
+            }
+        }
+
+        static void conversion() 
+        {
+            Console.WriteLine("enter speed");
+
+            double speed = Convert.ToDouble(Console.ReadLine());
+
+            speed = speed * 0.6213;
+
+            Console.WriteLine(speed);
+
+        }
+    }
+}
