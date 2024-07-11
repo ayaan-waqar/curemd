ee3b97e3469b5ce6457361134a3399fd718cf9cd
ayaan-waqar <143525054+ayaan-waqar@users.noreply.github.com>
2024-07-11 15:24:08 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,168 +1,64 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
-namespace assignment1
+namespace assignment2
 
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Sumofnumbers();
-            //Evenumbers();
-            //Leapyear();
-            //Conversion();
-            //Buzznumber();
-            //Table();
-            //Factorial();
-            Triangle();
-            //Palindrome();
+            Removeduplicate();
+            Largestnumber();
+
         }
 
-        static void Sumofnumbers()
+        static void Removeduplicate() 
         {
-            string sum;
-            Console.WriteLine("enter num1 and num2");
+            int[] numbers = new int[10];
 
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] unique = numbers.Distinct().ToArray();
+            Console.WriteLine("array after removing duplicate values");
 
-            int num3 = num1 + num2;
-            sum = num3.ToString();
-
-            Console.WriteLine(sum);
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.Write(unique[i]+ " , ");
+            }
 
 
         }
 
-        static void Evenumbers()
+        static void Largestnumber() 
         {
-            for (int i = 2; i <= 100; i++)
-            {
-                Console.WriteLine(i);
+            int[] numbers = new int[10];
 
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
             }
-        }
+            int largest = int.MinValue;
+            int seclargest = int.MinValue;
 
-        static void Leapyear()
-        {
-            Console.WriteLine("enter year");
-
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            if (year % 4 == 0)
+            foreach (int i in numbers)
             {
-                Console.WriteLine("it is a leap year");
-            }
-            else
-            {
-                Console.WriteLine("it is not a leap year");
-            }
-        }
+                if(i > largest) 
+                {
+                    seclargest = largest;
+                    largest = i;
+                }
 
-        static void Conversion()
-        {
-            Console.WriteLine("enter speed");
+                else if (i > seclargest && i != largest) 
+                {
+                    seclargest = i;
+                }
 
-            double speed = Convert.ToDouble(Console.ReadLine());
-
-            speed = speed * 0.6213;
-
-            Console.WriteLine(speed);
-
-        }
-
-        static void Buzznumber()
-        {
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            if (num % 7 == 0 || num % 10 == 7)
-            {
-                Console.WriteLine("it is a buzz number");
-            }
-            else
-            {
-                Console.WriteLine("it is not a buzz number");
+                Console.WriteLine("largest number is" + largest);
+                Console.WriteLine("second largest is" + seclargest);
             }
 
-        }
-
-        static void Table()
-        {
-            Console.WriteLine("enter number");
-
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine("{0}*{1} = {2}", num, i, num * i);
-            }
-        }
-
-        static void Factorial()
-        {
-            Console.WriteLine("enter number");
-
-            int num = Convert.ToInt32(Console.ReadLine());
-            int f = 1;
-            for (int i = 1; i <= num; i++)
-            {
-                f = f * i;
-            }
-            Console.WriteLine(f);
-        }
-
-        //static void Primenumber()
-        //{
-        //    Console.WriteLine("enter any number");
-        //    int num = Convert.ToInt32(Console.ReadLine());
-        //    int num2 = num - 1;
-        //    IsPrime = true;
-
-        //    while (num2 != 1) 
-        //    {
-        //        if (num / num2 == 0)
-        //        {
-        //            IsPrime = false
-        //        }
-              
-                
-        //            num2 = num2 - 1;
-                
-        //    }
-        //    if (IsPrime == true)
-        //    {
-        //        Console.WriteLine("prime");
-
-        //        else 
-        //        { 
-        //            (Console.WriteLine("Not prime"));
-
-        //        }
-        //    }
-
-        //}
-
-        static void Triangle() 
-        {
-            Console.WriteLine("enter lenght of sides of triangle");
-            int A = Convert.ToInt32(Console.ReadLine());
-            int B = Convert.ToInt32(Console.ReadLine());
-            int C = Convert.ToInt32(Console.ReadLine());
-
-            if (A != B && A != C && B != C)
-            {
-                Console.WriteLine("it is a scalene triangle");
-
-            }
-            else if (A == B || A == C || B == C)
-            {
-                Console.WriteLine("it is a isoceles triangle");
-            }
-            else if (A == B && B==C)
-            {
-                Console.WriteLine("it is an equilateral triangle");
-            }
         }
     }
 }
